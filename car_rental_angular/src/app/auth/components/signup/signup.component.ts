60ec71313959be3e1d837d1a80fd22baa8178e99
Lucas Lima <lucaserolima@gmail.com>
2024-04-06 20:37:42 -0400

    feat - Added 'checkPassword' validation and button will only be active if all fields are filled in and validated - Frontend

--- car_rental_angular/src/app/auth/components/signup/signup.component.ts
+++ car_rental_angular/src/app/auth/components/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { error } from 'console';
 
 @Component({
   selector: 'app-signup',
@@ -11,18 +12,28 @@ export class SignupComponent {
   isSpinning: boolean = false;
   signupForm!: FormGroup;
 
-  constructor(private fb: FormBuilder){}
+  constructor(private fb: FormBuilder) { }
 
-  ngOnInit(){
+  ngOnInit() {
     this.signupForm = this.fb.group({
       name: [null, [Validators.required]],
       email: [null, [Validators.required, Validators.email]],
       password: [null, [Validators.required]],
-      checkPassword: [null, [Validators.required]]
+      checkPassword: [null, [Validators.required, this.confirmationValidate]]
     })
   }
 
-  register(){
+  confirmationValidate = (control: FormControl): { [s: string]: boolean } => {
+    if (!control.value) {
+      return { required:true };
+    } else if (control.value !== this.signupForm.controls['password'].value) {
+      return { confirm: true, error: true };
+    }
+    return {};
+  }
+
+
+  register() {
     console.log(this.signupForm.value);
   }
 
