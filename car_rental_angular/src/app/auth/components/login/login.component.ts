2f3803cf9d94f6da497fa79fc94261419455514c
Lucas Lima <lucaserolima@gmail.com>
2024-04-08 20:52:47 -0400

    feat: Role-Based  (ADMIN or CUSTOMER) Navigation in the Application. Login moment. - Frontend

--- car_rental_angular/src/app/auth/components/login/login.component.ts
+++ car_rental_angular/src/app/auth/components/login/login.component.ts
@@ -1,6 +1,9 @@
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth/auth.service';
+import { StorageService } from '../../services/storage/storage.service';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-login',
@@ -8,27 +11,43 @@ import { AuthService } from '../../services/auth/auth.service';
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-
   isSpinning: boolean = false;
   loginForm!: FormGroup;
 
   constructor(
     private fb: FormBuilder,
-    private authService: AuthService
-  ){}
+    private authService: AuthService,
+    private router: Router,
+    private message: NzMessageService
+  ) {}
 
-  ngOnInit(){
+  ngOnInit() {
     this.loginForm = this.fb.group({
       email: [null, [Validators.email, Validators.required]],
-      password: [null, [Validators.required]]
-    })
+      password: [null, [Validators.required]],
+    });
   }
 
-  login(){
+  login() {
     console.log(this.loginForm.value);
     this.authService.login(this.loginForm.value).subscribe((res) => {
-      console.log(res)
-    })
+      console.log(res);
+      if (res.userId != null) {
+        const user = {
+          id: res.userId,
+          role: res.userRole,
+        };
+        StorageService.saveUser(user);
+        StorageService.saveToken(res.jwt);
 
+        if (StorageService.isAdminLoggedIn()) {
+          this.router.navigateByUrl('/admin/dashboard');
+        } else if (StorageService.isCustomerLoggedIn()) {
+          this.router.navigateByUrl('/customer/dashboard');
+        } else {
+          this.message.error('Bad Credentials', { nzDuration: 50000 });
+        }
+      }
+    });
   }
 }
