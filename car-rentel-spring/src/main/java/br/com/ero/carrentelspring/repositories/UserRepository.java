77e9c9e173e71fc1b450582333a9fc766fac8f5a
Lucas Lima <lucaserolima@gmail.com>
2024-04-08 20:14:26 -0400

    feat: Creating an Admin Account in Spring Boot - Backend

--- car-rentel-spring/src/main/java/br/com/ero/carrentelspring/repositories/UserRepository.java
+++ car-rentel-spring/src/main/java/br/com/ero/carrentelspring/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package br.com.ero.carrentelspring.repositories;
 
 import br.com.ero.carrentelspring.entities.User;
+import br.com.ero.carrentelspring.enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findFirstByEmail(String email);
+
+    User findByUserRole(UserRole userRole);
 }
