9b7db81165471c6e7238db130be20e1295b04fba
Lucas Lima <lucaserolima@gmail.com>
2024-04-11 16:56:31 -0400

    feat: Create a page past car - Frontend

---
+++ car_rental_angular/src/app/modules/admin/components/post-car/post-car.component.ts
@@ -0,0 +1,67 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-post-car',
+  templateUrl: './post-car.component.html',
+  styleUrl: './post-car.component.scss',
+})
+export class PostCarComponent {
+
+  postCarForm!: FormGroup | any;
+
+  isSpinning: boolean = false;
+  selectedFile: File;
+  imagePreview: string | ArrayBuffer | null;
+
+  listOfOption = Array<{label: string, value: string }>;
+  listOfBrands = ["BMW", "AUDI", "FERRARI", "TESLA", "VOLVO", "TOYOTA", "HONDA", "FORD", "NISSAN", "HYUNDAI", "LEXUS", "KIA"]
+  listOfType = ["Petrol", "Hybrid", "Diesel", "Electric", "CNG"];
+  listOfColor = ["Red", "White", "Blue", "Black", "Orange", "Grey", "Silver"];
+  listOfTransmission = ["Manual", "Automatic"];
+
+  constructor(private fb: FormBuilder){}
+
+  ngOnInit(){
+    this.postCarForm = this.fb.group({
+      name: [null, Validators.required],
+      brand: [null, Validators.required],
+      type: [null, Validators.required],
+      color: [null, Validators.required],
+      transmission: [null, Validators.required],
+      price: [null, Validators.required],
+      description: [null, Validators.required],
+      year: [null, Validators.required]
+    })
+  }
+
+  postCar(){
+    console.log(this.postCarForm.value);
+    const formData: FormData = new FormData();
+    formData.append('img', this.selectedFile);
+    formData.append('brand', this.postCarForm.get('brand').value);
+    formData.append('name', this.postCarForm.get('name').value);
+    formData.append('type', this.postCarForm.get('type').value);
+    formData.append('color', this.postCarForm.get('color').value);
+    formData.append('year', this.postCarForm.get('year').value);
+    formData.append('transmission', this.postCarForm.get('transmission').value);
+    formData.append('description', this.postCarForm.get('description').value);
+    formData.append('price', this.postCarForm.get('price').value);
+    console.log(formData);
+
+  }
+
+  onFileSelected(event: any){
+    this.selectedFile = event.target.files[0];
+    this.previewImage();
+  }
+
+  previewImage(){
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result;
+    }
+    reader.readAsDataURL(this.selectedFile);
+  }
+
+}
